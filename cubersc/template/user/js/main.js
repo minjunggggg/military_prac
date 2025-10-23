63c6dd3b31dd81dc6c6a355f530fbe19cfe9a238
minjung <togatecomz.mj@gmail.com>
2025-10-23 14:59:13 +0900

    팝업 수정

--- cubersc/template/user/js/main.js
+++ cubersc/template/user/js/main.js
@@ -1,46 +1,31 @@
 // 민정 추가
-$(function () {
-    const popup = $(".h-win");
-    const cookieName = "hidePopup";
+// 쿠키 세팅 및 쿠키에 따라 팝업 보여지기
+function setCookie(name, value, days) {
+    const d = new Date();
+    d.setDate(d.getDate() + days);
+    document.cookie = name + "=" + value + "; path=/; expires=" + d.toUTCString();
+}
+function getCookie(name) {
+    const match = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
+    return match ? match[2] : null;
+}
 
-    // --- 쿠키 존재하면 팝업 숨김 ---
-    if (getCookie(cookieName) === "Y") {
-        popup.hide();
+window.onload = function () {
+    if (getCookie("hidePopup") === "Y") {
+        $(".help-bg.beta-eventpop").hide();
+    } else {
+        $(".help-bg.beta-eventpop").show();
     }
+};
 
-    // --- 닫기 버튼 ---
-    $(".w-close").on("click", function (e) {
-        e.preventDefault();
-        popup.hide();
-    });
-
-    // --- 오늘 하루 보지 않기 ---
-    $('.btn_nottoday2 input[type="checkbox"]').on("change", function () {
-        if ($(this).is(":checked")) {
-            setCookie(cookieName, "Y", 1); // 1일 유효
-            popup.hide();
-        }
-    });
-
-    // --- 쿠키 함수 ---
-    function setCookie(name, value, days) {
-        const d = new Date();
-        d.setDate(d.getDate() + days);
-        document.cookie = `${name}=${value}; path=/; expires=${d.toUTCString()}`;
-    }
-
-    function getCookie(name) {
-        const v = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
-        return v ? v[2] : null;
-    }
-});
-
+// 팝업 닫기 버튼 클릭 시
 $(document).on("click", ".w-close", function (e) {
     e.preventDefault();
-    $(".h-win").hide();
+    $(".help-bg.beta-eventpop").hide();
 });
 
-$(document).on("change", '.btn_nottoday2 input[type="checkbox"]', function () {
+// 오늘 하루 보지 않기 버튼 클릭 시
+$(document).on("click", ".btn_nottoday2", function () {
     setCookie("hidePopup", "Y", 1);
-    $(".h-win").hide();
+    $(".help-bg.beta-eventpop").hide();
 });
